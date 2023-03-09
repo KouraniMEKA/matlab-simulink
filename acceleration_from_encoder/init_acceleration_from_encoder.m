26f2bedc294564eab4299e1f42c6dc880927a6a4
Ahmad Kourani <kourani.ahmad@gmail.com>
2023-03-09 14:37:41 +0200

    Add files via upload

---
+++ acceleration_from_encoder/init_acceleration_from_encoder.m
@@ -0,0 +1,8 @@
+
+sensor.n = 5;
+sensor.sigma = 0;
+sensor.range = 360;
+sensor.bits_resolution = 12;
+sensor.resolution = sensor.range/2^sensor.bits_resolution;
+sensor.sample_time = 1e-3;
+sensor.estimation_time = 1e-3;
